ea9c2de9c5b69244ff9e7b05333311e4c60a10d0
jevans <jevans@degdigital.com>
2015-04-18 16:23:18 -0700

    Add empty FactoryTest.php

--- app/code/community/DEG/OrderLifecycle/Model/Lifecycle/Event.php
+++ app/code/community/DEG/OrderLifecycle/Model/Lifecycle/Event.php
@@ -1,4 +1,1 @@
-Thing Happens in Module1 -> Dispatches Event
-Observer in Module1 listens and catches dispatch
-Observer in Module1 would create a data array with data it wants to save and dispatches "save_lifecycle_event" with data array
-Observer in OrderLifecycle listens and creates "Lifecycle/Event" and saves to registry
+<?php
---
+++ app/code/community/DEG/OrderLifecycle/Test/Model/Lifecycle/Event/FactoryTest.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jevans
+ * Date: 4/18/15
+ * Time: 6:20 PM
+ */
