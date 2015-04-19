10a1e0b2db0993e8800bab977822091fe96f41ca
Ben Robie <brobie@degdigital.com>
2015-04-18 21:12:56 -0700

    Changing Hook and fixing bug in event saving.

--- app/code/community/DEG/OrderLifecycle/Model/Hooks.php
+++ app/code/community/DEG/OrderLifecycle/Model/Hooks.php
@@ -2,14 +2,22 @@
 
 class DEG_OrderLifecycle_Model_Hooks {
 
-    public function orderBeforeSaveLifecycleEvent($event){
+    public function orderAfterSaveLifecycleEvent($event){
         $order = $event->getDataObject();
         $newState = $order->getData('state');
         $oldState = $order->getOrigData('state');
-        if ($newState != $oldState) {
+        $newStatus = $order->getData('status');
+        $oldStatus = $order->getOrigData('status');
+        if (($newState != $oldState) || ($newStatus != $oldStatus)) {
             $object = new Varien_Object();
-            $object->setOldState($oldState);
-            $object->setNewState($newState);
+            if (($newState != $oldState)) {
+                $object->setOldState($oldState);
+                $object->setNewState($newState);
+            }
+            if (($newStatus != $oldStatus)) {
+                $object->setOldStatus($oldState);
+                $object->setNewStatus($newState);
+            }
             Mage::dispatchEvent('lifecycle_event', array('data_object' => $object));
         }
     }
--- app/code/community/DEG/OrderLifecycle/Model/Observer.php
+++ app/code/community/DEG/OrderLifecycle/Model/Observer.php
@@ -5,12 +5,12 @@ class DEG_OrderLifecycle_Model_Observer {
     public function lifecycleEventToRegistry($event){
         $lifecycleEvent = Mage::getSingleton('deg_orderlifecycle/lifecycle_event_factory')->getEventDataObject();
         $eventData = $event->getDataObject();
-        $lifecycleEvent->setData($eventData->getData());
+        $lifecycleEvent->addData($eventData->getData());
         $collection = Mage::registry(DEG_OrderLifecycle_Model_Lifecycle_Event_Collection::REGISTRY_LIFECYCLE_EVENT_COLLECTION);
         if (!$collection){
             $collection = Mage::getModel('deg_orderlifecycle/lifecycle_event_collection');
         }
-        $collection->addEvent($event);
+        $collection->addEvent($lifecycleEvent);
         Mage::unregister(DEG_OrderLifecycle_Model_Lifecycle_Event_Collection::REGISTRY_LIFECYCLE_EVENT_COLLECTION);
         Mage::register(DEG_OrderLifecycle_Model_Lifecycle_Event_Collection::REGISTRY_LIFECYCLE_EVENT_COLLECTION, $collection);
     }
