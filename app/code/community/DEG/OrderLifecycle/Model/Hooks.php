34771c3daaedcc3e6c3f397a70a46334436e77f7
Ben Robie <brobie@degdigital.com>
2015-04-19 10:32:14 -0700

    Adding payment, invoice, shippment hooks

--- app/code/community/DEG/OrderLifecycle/Model/Hooks.php
+++ app/code/community/DEG/OrderLifecycle/Model/Hooks.php
@@ -17,4 +17,29 @@ class DEG_OrderLifecycle_Model_Hooks {
     public function preDispatch($event){
         Mage::dispatchEvent('lifecycle_event', array('data_object' => new Varien_Object()));
     }
+
+    public function paymentCaptureLifecyceEvent($event){
+        $object = new Varien_Object();
+        $object->setMessage('Captured Order');
+        Mage::dispatchEvent('lifecycle_event', array('data_object' => $object));
+    }
+
+    public function paymentRefundLifecyceEvent($event){
+        $object = new Varien_Object();
+        $object->setMessage('Refunded Order');
+        Mage::dispatchEvent('lifecycle_event', array('data_object' => $object));
+    }
+
+    public function invoiceCreatedLifecyceEvent($event){
+        $object = new Varien_Object();
+        $object->setMessage('Invoice Created');
+        Mage::dispatchEvent('lifecycle_event', array('data_object' => $object));
+    }
+
+    public function shippmentCreatedLifecyceEvent($event){
+        $object = new Varien_Object();
+        $object->setMessage('Shippment Created');
+        Mage::dispatchEvent('lifecycle_event', array('data_object' => $object));
+    }
+
 }
